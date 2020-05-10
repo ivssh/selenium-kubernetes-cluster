2841cdb9ed0e8027e00c8654a5566d4bb240d698
Abhilash <abhilash@Admins-MacBook-Pro.local>
2020-05-10 18:14:39 +0530

    Changed IP of the Selenium Hub

--- python_tests/google_test.py
+++ python_tests/google_test.py
@@ -4,7 +4,7 @@ desiredCapabilities={
 "browserName":"chrome"
 }
 
-driver = webdriver.Remote(command_executor='http://34.67.131.205:4444/wd/hub',desired_capabilities = desiredCapabilities)
+driver = webdriver.Remote(command_executor='http://localhost:4444/wd/hub',desired_capabilities = desiredCapabilities)
 driver.get("https://www.google.co.in/")
 print(driver.title)
 driver.quit()
